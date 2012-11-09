0e211a1b1e2d18a436fc907fe4ee39f2b6525066
Martin Povolny <mpovolny@redhat.com>
2012-11-09 21:10:06 +0100

    make taskomatic create instances of InstanceHwp

--- src/app/util/taskomatic.rb
+++ src/app/util/taskomatic.rb
@@ -149,6 +149,17 @@ module Taskomatic
 
     overrides = HardwareProfile.generate_override_property_values(instance.hardware_profile, match.hardware_profile)
 
+    #ihwp = InstanceHwp.create( overrides ) FIXME
+    ihwp = InstanceHwp.create(
+      :memory       => overrides[:memory],
+      :cpu          => overrides[:cpu],
+      :architecture => overrides[:architecture],
+      :storage      => overrides[:storage],
+    )
+    ihwp.hardware_profile = match.hardware_profile
+    instance.instance_hwp = ihwp
+    ihwp.save
+
     client_args = {:image_id => match.provider_image,
                   :name => instance.name.tr("/", "-"),
                   :hwp_id => match.hardware_profile.external_key,
